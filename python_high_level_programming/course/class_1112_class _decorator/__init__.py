4890d6ed0ab830d93ba03e8455db9fd5e52ef50d
liuzhipeng612 <57635630+liuzhipeng612@users.noreply.github.com>
2019-11-13 23:09:45 +0800

    Class decorator

---
+++ python_high_level_programming/course/class_1112_class _decorator/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author: Jomer
+@Contact: jomer3126@gmail.com
+@Project: Python_Advanced_Software_Test_Development_Course
+@File: __init__.py.py
+@Time: 2019/11/13 23:09
+@Desc: Jungle old monster 
+"""
