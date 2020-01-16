7af0ee44458c6bf88d70aadd8658a43f250a0c31
liuzhipeng612 <57635630+liuzhipeng612@users.noreply.github.com>
2020-01-16 16:34:56 +0800

    练习

--- test_project/django_rest_framework/django_views/models.py
+++ test_project/django_rest_framework/django_views/models.py
@@ -17,3 +17,14 @@ class Projects(models.Model):
     desc = models.CharField(verbose_name="简要描述", help_text="简要描述", max_length=200, default="", blank=True, null=True)
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间", help_text="创建时间")
     update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间", help_text="更新时间")
+
+
+class TestTeam(models.Model):
+    id = models.AutoField(verbose_name="id主键", primary_key=True, help_text="id主键")
+    name = models.CharField(verbose_name="团队名称", help_text="团队名称", max_length=200)
+    leader = models.CharField(verbose_name="负责人", help_text="负责人", max_length=50)
+    tester = models.CharField(verbose_name="测试人员", help_text="测试人员", max_length=50)
+    modulename = models.CharField(verbose_name="模块名称", help_text="模块名称", max_length=200)
+    desc = models.CharField(verbose_name="简要描述", help_text="简要描述", max_length=200, default="", blank=True, null=True)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间", help_text="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间", help_text="更新时间")
