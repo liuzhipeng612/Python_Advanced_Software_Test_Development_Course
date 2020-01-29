64039987f32b5ad033e3b10d7d05a2cf930846bb
Daodao <57635630+liuzhipeng612@users.noreply.github.com>
2020-01-29 15:38:41 +0800

    rest framework

--- django_framework/projects/views.py
+++ django_framework/projects/views.py
@@ -55,29 +55,29 @@ class IndexView(View):
         # # 1、如果没有查询到记录会报错，如果查询到多个记录也会报错，只有返回一条记录才不会报错
         # # 2、返回的是模型类对象
         # # 3、get方法最好使用主键或者唯一键去查询
-        qs = Projects.objects.get(id=1)
+        # qs = Projects.objects.get(id=1)
 
         # 三、获取多条记录，使用filter()
-        qs=Projects.objects.filter(id=1)
-        qs=Projects.objects.filter(leader__contains="某人")   #一条数据的某字段中包含xx内容
-        qs = Projects.objects.filter(name__contains=["项目1", "项目2"])  # 查看分别在集合中对应的项目
+        # qs=Projects.objects.filter(id=1)
+        # qs=Projects.objects.filter(leader__contains="某人")   #一条数据的某字段中包含xx内容
+        # qs = Projects.objects.filter(name__contains=["项目1", "项目2"])  # 查看分别在集合中对应的项目
 
         # 四、联表查询
         # 将多个条件在同一个filter中指定，为“与”的关系
-        qs=Projects.objects.filter(name__contains="项目",id__gt=10)
+        # qs=Projects.objects.filter(name__contains="项目",id__gt=10)
         # 一个QuerySet对象支持链式操作，可以同时调用多个filter
-        qs = Projects.objects.filter(name__contains="项目").filter(id__gt=10)
+        # qs = Projects.objects.filter(name__contains="项目").filter(id__gt=10)
         # 或的关系需要时用Q变量
-        qs = Projects.objects.filter(Q(name__contains="项目") | Q(id__gt=10))
+        # qs = Projects.objects.filter(Q(name__contains="项目") | Q(id__gt=10))
 
         # 五、多表关联查询
         # 使用从表的条件来查找父表的数据
         # 使用从表模型类名小写__从表字段名__条件运算符
-        qs=Projects.objects.filter(interfaces__name__contains="注册")
+        # qs=Projects.objects.filter(interfaces__name__contains="注册")
 
         # 六、获取查询集对象的数量
         # 使用查询集对象调用count()方法获取查询集中的数据条数
-        qs = Projects.objects.filter(name__contains="项目").count()
+        # qs = Projects.objects.filter(name__contains="项目").count()
 
         # 七、排序操作
         # 可以使用查询集对象调用order_by()方法来对查询集进行排序
