e4881109b8660d32979f64f64be6a83c5b3ad5ed
Daodao <57635630+liuzhipeng612@users.noreply.github.com>
2020-03-20 01:23:59 +0800

    格式化views.py文件

--- django_framework/projects/views.py
+++ django_framework/projects/views.py
@@ -1,12 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
-from rest_framework import mixins, viewsets
+# from rest_framework import mixins
+from rest_framework import viewsets
 from rest_framework.response import Response
 
 from projects.models import Projects
 from . import serializers
-
 
 # class ProjectList(generics.ListCreateAPIView):
 #     queryset = Projects.objects.all()
@@ -20,13 +20,13 @@ from . import serializers
 #     queryset = Projects.objects.all()
 #     serializer_class = serializers.ProjectModelSerializer
 
-
 # class ProjectViewSet(mixins.CreateModelMixin,
 #                      mixins.ListModelMixin,
 #                      mixins.RetrieveModelMixin,
 #                      mixins.UpdateModelMixin,
 #                      mixins.DestroyModelMixin,
 #                      viewsets.GenericViewSet):
+
 
 class ProjectViewSet(viewsets.ModelViewSet):
     """
